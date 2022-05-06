f8e9be3313ca653d86bda46b8cdec9693ad995c6
Omer Tuchfeld <omer@tuchfeld.dev>
2022-05-06 18:01:25 +0200

    Updated script

--- failures.sh
+++ failures.sh
@@ -16,7 +16,6 @@ ls /root/omer/manifests/ | xargs -I % sh -c "echo %; oc get secret %-admin-kubec
 # List failures
 oc get aci -A -ojson | jq '.items[] | select((.status.conditions[] | select(.type == "Failed")).status == "True") | .metadata.name' -r > faillist
 
-# Crashes for 4.10.13 and above
 for cluster in $(cat faillist); do
 	export KUBECONFIG=manifests/$cluster/kubeconfig
     if ! oc get pods 2>/dev/null >/dev/null; then
@@ -57,66 +56,11 @@ for cluster in $(cat faillist); do
         continue
     fi
     if ! oc logs -n openshift-kube-controller-manager kube-controller-manager-$cluster kube-controller-manager 2>/dev/null 1>/dev/null; then
-        echo DeadKubeControllerManager $cluster
+        echo DeadKubeControllerManager-Bz2082628 $cluster
         continue
     fi
     if [[ $(oc get pods -A -ojson | jq '[.items[] | select(.status.phase == "Pending").metadata.name] | length') > 10 ]]; then
         echo LotsOfPending $cluster
-        continue
-    fi
-    if [[ $(oc get co -ojson | jq -r '[.items[] | select((.status.conditions[]? | select(.type == "Available").status) == "False").metadata.name] | length') == "0" ]]; then
-        echo EventuallyOkay $cluster
-        continue
-    fi
-    echo Else $cluster
-done
-
-# Crashes for 4.10.12 and below
-for cluster in $(cat faillist); do
-	export KUBECONFIG=manifests/$cluster/kubeconfig
-    if ! oc get pods 2>/dev/null >/dev/null; then
-        if ssh core@$cluster sudo crictl logs $(ssh core@$cluster sudo crictl ps -a --name '^kube-apiserver$' -q 2>/dev/null | head -1) 2>&1 | grep "ca-bundle.crt: no such file or directory" -q; then
-            echo Bz2017860 $cluster
-            continue
-        fi
-
-        echo Offline $cluster
-        continue
-    fi
-    if oc get pods -A | grep -E '(openshift-apiserver|openshift-authentication|openshift-oauth-apiserver|package-server-manager|cluster-storage-operator-)' | grep -q Crash; then
-        echo BadOVN $cluster
-        continue
-    fi
-	if oc get pods -A | grep openshift-authentication | grep -q Crash; then
-        echo BadOVN $cluster
-        continue
-    fi
-    if oc get pods -n openshift-authentication 2>/dev/null | grep -q ContainerCreating; then
-        echo BadOVN $cluster
-        continue
-    fi
-    if oc get co monitoring -ojson  | jq '.status.conditions[]? | select(.type == "Degraded").message' -r | grep 'Grafna Deployment failed' -q; then
-        echo BadOVN $cluster
-        continue
-    fi
-	if oc get co machine-config  -ojson | jq '.status.conditions[]? | select(.type=="Degraded").message' | grep -q "waitForControllerConfigToBeCompleted"; then
-        echo WeirdMCO $cluster
-        continue
-    fi
-	if oc get co machine-config  -ojson | jq '.status.conditions[]? | select(.type=="Degraded").message' | grep -q "waitForDeploymentRollout"; then
-        echo WeirdMCO2 $cluster
-        continue
-    fi
-    if [[ $(oc get co -ojson | jq -r '[.items[] | select((.status.conditions[]? | select(.type == "Available").status) == "False").metadata.name] | length') == "1" && $(oc get co -ojson | jq -r '.items[] | select((.status.conditions[]? | select(.type == "Available").status) == "False").metadata.name') == "console" ]]; then
-        echo BadConsole $cluster
-        continue
-    fi
-    if ! oc logs -n openshift-kube-controller-manager kube-controller-manager-$cluster kube-controller-manager 2>/dev/null 1>/dev/null; then
-        echo DeadKubeControllerManager $cluster
-        continue
-    fi
-    if [[ $(oc get pods -A -ojson | jq '[.items[] | select(.status.phase == "Pending").metadata.name] | length') > 10 ]]; then
-        echo BadOVNMaybe-LotsOfPending $cluster
         continue
     fi
     if [[ $(oc get co -ojson | jq -r '[.items[] | select((.status.conditions[]? | select(.type == "Available").status) == "False").metadata.name] | length') == "0" ]]; then
