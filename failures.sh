3fdc8c4a3a8e6a1832a68100c6ec2b165b287c51
Omer Tuchfeld <omer@tuchfeld.dev>
2023-04-24 18:04:51 +0200

    EtcdLeaseStuck

--- failures.sh
+++ failures.sh
@@ -11,12 +11,12 @@ mkdir /root/omer/manifests -p
 cd /root/omer
 
 for n in $(seq -f "%05g" 1 3500); do
-    mkdir /root/omer/manifests/vm${n} -p
+	mkdir /root/omer/manifests/vm${n} -p
 done
 
 # List failures
 export KUBECONFIG=/root/bm/kubeconfig
-oc get aci -A -ojson | jq '.items[] | select((.status.conditions[] | select(.type == "Failed")).status == "True") | .metadata.name' -r > faillist
+oc get aci -A -ojson | jq '.items[] | select((.status.conditions[] | select(.type == "Failed")).status == "True") | .metadata.name' -r >faillist
 
 # Dump kubeconfigs
 export KUBECONFIG=/root/bm/kubeconfig
@@ -24,17 +24,29 @@ cat faillist | xargs -I % sh -c "echo %; oc get secret %-admin-kubeconfig -n % -
 
 export KUBECONFIG=/root/bm/kubeconfig
 for cluster in $(cat faillist); do
-    echo "$cluster"
-	export KUBECONFIG=manifests/$cluster/kubeconfig
-    oc get co
+    export KUBECONFIG=manifests/$cluster/kubeconfig
+    if oc get co -ojson 2>/dev/null | jq '
+            [
+                .items[] 
+                | select(
+                    .status.conditions[] 
+                    | select(.type == "Available").status == "False"
+                )
+            ] as $x 
+            | (($x | length) == 1 and $x[0].metadata.name == "etcd")
+        '>/dev/null 2>/dev/null; then
+         echo EtcdLeaseStuck "$cluster"
+    else
+        echo Other "$cluster"
+    fi
 done
 
 # Event scanning example
 cd /root/omer
 export KUBECONFIG=/root/bm/kubeconfig
 cat faillist | while read -r x; do
-    echo $x
-    curl -s -k $(oc get aci -n $x $x -ojson | jq '.status.debugInfo.eventsURL' -r) | jq -r '
+	echo $x
+	curl -s -k $(oc get aci -n $x $x -ojson | jq '.status.debugInfo.eventsURL' -r) | jq -r '
         .[] 
         | select(
             (.message | test("connected"))
